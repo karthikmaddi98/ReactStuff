60e95469cf13363d97dc89178209496f5eded3ae
Karthik Maddi <karthikmaddi98@gmail.com>
2024-08-09 20:35:08 +0530

    My First Learnings in React

---
+++ App.js
@@ -0,0 +1,35 @@
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    [
+        React.createElement(
+            "div", {id: "child"},
+            [
+                React.createElement("h1", {}, "Direct Div creation in App.js!!"),
+                React.createElement("h2",{},"Array of Children!!")
+            ]
+        ),
+        React.createElement(
+            "div", {id: "child2"},
+            [
+                React.createElement("h1", {}, "Div2 Data"),
+                React.createElement("h2",{},"Array of Children in Div2!!")
+            ]
+        )
+    ]
+);
+
+
+// const heading = React.createElement(
+//     "h1", 
+//     {id: "heading"}, 
+//     "Hello World from React!!"
+// );
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//console.log(root);
+root.render(parent);
